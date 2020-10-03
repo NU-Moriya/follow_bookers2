987f9bcc9ac881fb4874cf85d4d623d4bfd70e58
nobuko.m <animopara100pre@gmail.com>
2020-10-03 22:52:59 +0900

    Implement follow_follower

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update] do
-    resouces :relationships, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
   end
